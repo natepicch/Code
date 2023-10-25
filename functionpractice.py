5a17ab13fde5e3f038d0af301ea63e266edeed0c
natepicch <149009865+natepicch@users.noreply.github.com>
2023-10-25 15:28:48 -0400

    Update functionpractice.py

--- functionpractice.py
+++ functionpractice.py
@@ -1,3 +1,11 @@
+#lesseroftwo
+def lesser_of_the_two(a,b):
+    if a%2 == 0 and b%2 == 0 and a < b: 
+        return a
+    else: 
+        return b
+
+#makeinputletter1and4cap
 def cap(name): 
     i = 0 
     a = '' 
@@ -10,3 +18,15 @@ def cap(name):
         i+=1 
         
     return a.join(b)
+#Given a list of ints, return True if the array contains a 3 next to a 3 somewhere
+def ints(input):
+    for i in range(0,len(input)-1): #goes up to len(input)-1 becauseif it went to the end it would compare the last number with i+1 being nothing
+        if i == 3 and i + 1 == 3:
+            return 'True'
+        else:
+            return 'False'
+
+
+    
+    
+
